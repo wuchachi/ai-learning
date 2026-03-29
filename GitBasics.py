e5edb966272f795ffda642da14834c753beac8c7
wuchachi <elliotpark04022001@gmail.com>
2026-03-29 03:06:45 -0400

    Added new file, gitbasics

---
+++ GitBasics.py
@@ -0,0 +1,7 @@
+#Manually push this file to Git
+#Everyday workflow for Git
+
+#git add .                                           -------- adds all files in the snapshot
+#git commit -m "describe what you actually changed"  -------- snapshot with message
+#git push                                            -------- pushes to GitHub
+
